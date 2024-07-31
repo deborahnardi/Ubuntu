95516aed8b716c5d67d449333fc6001082658eec
Deborah <deborahnardi@usp.br>
2024-07-31 08:52:33 -0300

    ShapeFunctions

--- FEM/headers/Elements/ShapeFunction.h
+++ FEM/headers/Elements/ShapeFunction.h
@@ -33,3 +33,80 @@ public:
     If override is not written, the base version will be used
     */
 };
+
+class SF_L3 : public ShapeFunction
+{
+private:
+
+public:
+
+    SF_L3();
+    ~SF_L3();
+
+    void evaluateShapeFunction(std::vector<double> &xi) override;
+};
+class SF_L4 : public ShapeFunction
+{
+private:
+
+public:
+
+    SF_L4();
+    ~SF_L4();
+
+    void evaluateShapeFunction(std::vector<double> &xi) override;
+};
+
+class SF_T3 : public ShapeFunction
+{
+private:
+
+public:
+
+    SF_T3();
+    ~SF_T3();
+
+    void evaluateShapeFunction(std::vector<double> &xi) override;
+};
+
+class SF_T6 : public ShapeFunction
+{
+private:
+
+public:
+
+    SF_T6();
+    ~SF_T6();
+
+    void evaluateShapeFunction(std::vector<double> &xi) override;
+};
+
+class SF_T10 : public ShapeFunction
+{
+private:
+
+public:
+
+    SF_T10();
+    ~SF_T10();
+
+    void evaluateShapeFunction(std::vector<double> &xi) override;
+};
+
+class SF_TET10 : public ShapeFunction
+{
+public:
+    SF_TET10();
+    ~SF_TET10();
+
+    void evaluateShapeFunction(std::vector<double> &xi) override;
+};
+
+class SF_TET4 : public ShapeFunction
+{
+public:
+    SF_TET4();
+    ~SF_TET4();
+
+    void evaluateShapeFunction(std::vector<double> &xi) override;
+};
